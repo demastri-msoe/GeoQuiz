8e8f45590cf35894c501e74dbc8b05b9b6c41cda
John DeMastri <demastri@msoe.edu>
2023-07-26 16:46:43 -0500

    Chapter 1 Challenge - switch Toast for Snackbar

--- app/src/main/java/com/msoe/bnrtextapps/geoquiz/MainActivity.kt
+++ app/src/main/java/com/msoe/bnrtextapps/geoquiz/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
@@ -19,19 +20,18 @@ class MainActivity : AppCompatActivity() {
 
         trueButton.setOnClickListener { view: View ->
             // Do something in response to the click here
-            Toast.makeText(
-                this,
+            Snackbar.make(
+                view,
                 R.string.correct_toast,
-                Toast.LENGTH_SHORT
+                Snackbar.LENGTH_LONG
             ).show()
         }
         falseButton.setOnClickListener { view: View ->
             // Do something in response to the click here
-            Toast.makeText(
-                this,
+            Snackbar.make(
+                view,
                 R.string.incorrect_toast,
-                Toast.LENGTH_SHORT
-            ).show()
-        }
+                Snackbar.LENGTH_LONG
+            ).show()        }
     }
 }
